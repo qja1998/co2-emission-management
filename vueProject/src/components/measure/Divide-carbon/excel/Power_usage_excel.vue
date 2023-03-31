48434fc9e1ef92a4ba98a3f5d55c35f8b68f5ce3
hyemijeong <gpal6072@naver.com>
2023-03-31 17:31:23 +0900

    전력 카테고리 엑셀 일괄등록

--- vueProject/src/components/measure/Divide-carbon/excel/Power_usage_excel.vue
+++ vueProject/src/components/measure/Divide-carbon/excel/Power_usage_excel.vue
@@ -4,6 +4,7 @@
           <div style="font-size:2vh; font-weight:normal; margin-top:4vh">엑셀파일을 이용하여 전력 사용에 대한 데이터를 한번에 등록할 수 있습니다.<br>
             정해진 엑셀 양식을 다운로드 받아 양식에 따라 데이터를 작성 후 엑셀 파일을 업로드하면 전력 사용 내역이 일괄 등록됩니다.
           </div>
+
             <section>
                 <xlsx-workbook>
                   <xlsx-sheet
@@ -13,29 +14,31 @@
                       :sheet-name="sheet.name"
                   />
                   <xlsx-download>
-                      <button class="excel-down-btn" style="">엑셀 양식 다운로드</button>
+                      <button class="excel-down-btn">엑셀 양식 다운로드</button>
                   </xlsx-download>
                 </xlsx-workbook>
             </section>
 
             <div style="border:1px solid #D4D4D4; padding:3vh 2vw 2vw 5vh; margin-top:5vh; border-radius: 1vh;">
               <span style="font-size:2vh; font-weight:bold; margin-right:2vw"> 파일 선택 </span>
-              <section style="font-size:2vh; display:inline-block">
-                <input style="font-size:2vh;" type="file"  @change="onChange" />
-                <xlsx-read :file="file">
-                <xlsx-table :sheet="selectedSheet" />
+              <input type="file" @change="onChange" />
+              <xlsx-read :file="file">
+                <xlsx-sheets>
+                  <template #default="{sheets}">
+                    <select v-model="selectedSheet">
+                      <option v-for="sheet in sheets" :key="sheet" :value="sheet">
+                        {{ sheet }}
+                      </option>
+                    </select>
+                  </template>
+                </xlsx-sheets>
                 <xlsx-json :sheet="selectedSheet">
-                    <template #default="{collection}">
-                    <div>
-                        {{ collection }}
-                    </div>
-                    </template>
+                  <template #default="{collection}">
+                    <button class ="clickbtn" style="float:right; margin-top:5vh" @click="registerExcel(collection)">엑셀 등록하기</button>
+                  </template>
                 </xlsx-json>
-                </xlsx-read>
-              </section>
+              </xlsx-read>
             </div>
-
-            <button class ="clickbtn" style="float:right; margin-top:5vh">엑셀 등록하기</button>
             
         </div>
       
@@ -47,7 +50,8 @@
   
   <script>
   import { XlsxRead, XlsxTable, XlsxSheets, XlsxJson, XlsxWorkbook, XlsxSheet, XlsxDownload } from "/home/hyemi/carbon/co2-emission-management/vueProject/node_modules/vue3-xlsx"
-  
+  import {useStore} from 'vuex'
+  import {ref} from 'vue'
   export default {
     components: {
       XlsxRead,
@@ -58,23 +62,66 @@
       XlsxSheet,
       XlsxDownload
     },
-    data() {
-      return {
-        file: null,
-        selectedSheet: null,
-        sheetName: null,
-        sheets: [{ name: "전력사용양식", data: [{ 탄소배출내용: null, 시작날짜:null, 종료날짜:null, 배출시설명:null, 운영주체:null,공급처:null,전력사용량:null}] }],
-        collection: [{ a: 1, b: 2 }]
-      };
+
+    setup(){
+      var file= ref(null)
+      var selectedSheet=ref(null)
+      var sheetName=ref(null)
+      var sheets=ref([
+        { name: "전력사용양식", 
+          data: [
+            { 탄소배출내용: String, 시작날짜: Date, 종료날짜:Date, 배출시설명:null, 운영주체:null,공급처:null,전력사용량:Number}
+          ] 
+        }
+      ])
+      var collection=ref([{a:1, b:2}])
+      var store = useStore()
+
+      function onChange(event){
+        file.value = event.target.files ? event.target.files[0] : null;
+      }
+
+      function addSheet(){
+        sheets.push({ name: this.sheetName, data: [...this.collection] });
+        sheetName.value = '전력사용양식';
+      }
+      function registerExcel(sheets){
+        var i
+        for(i=0; i<sheets.length; i++){
+          console.log(i)
+          var info_list={
+            Type:"7",
+            DetailType:"전력",
+            StartDate: sheets[i].시작날짜,
+            EndDate:sheets[i].종료날짜,
+            Location:"",
+            scope:2,
+            data:sheets[i].전력사용량,
+            emissions:sheets[i].전력사용량+4,
+            Carbonunit:"kg",
+            CarbonActivity:sheets[i].탄소배출내용,
+            kind:"",
+            Division:{건물명:"",운영주체:"",공급처:""}
+          }
+          console.log(info_list)
+          
+          store.commit("SetTableContent",info_list)
+        }
+      }
+      return{
+        file,
+        selectedSheet,
+        sheetName,
+        sheets,
+        collection,
+        onChange,
+        addSheet,
+        registerExcel
+      }
     },
     methods: {
-      onChange(event) {
-        this.file = event.target.files ? event.target.files[0] : null;
-      },
-      addSheet() {
-        this.sheets.push({ name: this.sheetName, data: [...this.collection] });
-        this.sheetName = '전력사용양식';
-      }
+
+      
     }
   };
   </script>
