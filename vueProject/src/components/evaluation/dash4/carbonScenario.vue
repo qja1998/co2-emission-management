7d66b20ee9be809def8c5d9263f96946fb07b36d
Hyemi <62432916+hyemijeong@users.noreply.github.com>
2023-06-10 22:29:52 +0900

    최최종

--- vueProject/src/components/evaluation/dash4/carbonScenario.vue
+++ vueProject/src/components/evaluation/dash4/carbonScenario.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="float:left; ">
       <div class="dash_title">탄소 중립 시나리오 추천</div>
-      <div class="dashboard" id="evaluation-dash4" onclick="location.href='/evaluation';" >
+      <div class="dashboard" id="evaluation-dash4" onclick="location.href='/scenario';" >
             <img style="margin: 5vh 0 0 6.5vw; width: 50%;" src="@/assets/carbonScenario.png" alt="탄소 중립 시나리오 추천">
             <div>
                 <img class="check" src="@/assets/check.png" alt="check">
